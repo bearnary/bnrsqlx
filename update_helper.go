e7fe3385a608e5a44ec32e5261a855adab83cf8d
Jirawat Harnsiriwatanakit <jirawat.h@kazekim.com>
2024-07-03 01:53:35 +0700

    add update array func

--- update_helper.go
+++ update_helper.go
@@ -3,6 +3,10 @@ package bnrsqlx
 import (
 	"errors"
 	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type UpdateHelper struct {
@@ -81,6 +85,62 @@ func (h *UpdateHelper) CommitUpdateQuery(tableName string) error {
 	return nil
 }
 
+func (h *UpdateHelper) CommitUpdateInQuery(tableName string) error {
+
+	h.paramCount = 1
+	var args []interface{}
+
+	if !h.HasChanged() {
+		return nil
+	}
+
+	whereQuery := []string{}
+	if len(h.whereParams) > 0 {
+		for key, value := range h.whereParams {
+			if isArrayOrSlice(value) {
+				inQuery, inArgs, err := sqlx.In(fmt.Sprintf("%v in (?)", key), value)
+				if err != nil {
+					return errors.New("no where clause in database query")
+				}
+				whereQuery = append(whereQuery, inQuery)
+				args = append(args, inArgs...)
+				h.paramCount += len(inArgs)
+			} else {
+				whereQuery = append(whereQuery, fmt.Sprintf("%v = $%d", key, h.paramCount))
+				args = append(args, value)
+				h.paramCount++
+			}
+		}
+	} else {
+		return errors.New("no where clause in database query")
+	}
+
+	updateQuery := []string{}
+	//isNotFirstUpdateClause := false
+	for key, value := range h.params {
+		updateQuery = append(updateQuery, fmt.Sprintf("%v = $%d", key, h.paramCount))
+		args = append(args, value)
+		h.paramCount++
+	}
+
+	whereSQL := strings.Join(whereQuery, " AND ")
+	updateSQL := strings.Join(updateQuery, ", ")
+
+	query := fmt.Sprintf("update %v set %v where %v", tableName, updateSQL, whereSQL)
+
+	query = h.db.DB().Rebind(query)
+
+	res, err := h.db.DB().Exec(query, args...)
+	if err != nil {
+		return err
+	}
+	_, err = res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (h *UpdateHelper) ParamArgumentVariable() string {
 	if h.db.DatabaseType() == DatabaseTypePostgres {
 		v := fmt.Sprintf("$%d", h.paramCount)
@@ -93,4 +153,12 @@ func (h *UpdateHelper) ParamArgumentVariable() string {
 
 func (h *UpdateHelper) HasChanged() bool {
 	return len(h.params) > 0
+}
+
+func isArrayOrSlice(input interface{}) bool {
+	typeOf := reflect.TypeOf(input)
+	if typeOf == nil {
+		return false
+	}
+	return typeOf.Kind() == reflect.Array || typeOf.Kind() == reflect.Slice
 }
